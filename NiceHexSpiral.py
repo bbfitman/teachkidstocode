cbe8195077c4b8ebd2704c1f11fda603d76ea7e7
bbfitman <bbfitman@sina.com>
2024-07-09 16:50:21 +0800

    Update for github test

--- NiceHexSpiral.py
+++ NiceHexSpiral.py
@@ -2,10 +2,11 @@
 import turtle   
 colors=['red', 'purple', 'blue',
         'green', 'yellow', 'orange']
+sides = 2 # this is a test line for github
 t=turtle.Pen()
 turtle.bgcolor('black')
 for x in range(360):
     t.pencolor(colors[x%6])
-    t.width(x/100+1)
-    t.forward(x)        
+    t.width(x*3/sides+x) # this is another changed line 
+    t.forward(360/sides+1) #the 3rd changed place        
     t.left(59)
